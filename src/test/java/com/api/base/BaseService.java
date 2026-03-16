0dee0773a3699a427bc2754fb753eadebc743620
shilpatailor <shilpatailor4@gmail.com>
2026-03-16 06:16:45 +0000

    update

--- src/test/java/com/api/base/BaseService.java
+++ src/test/java/com/api/base/BaseService.java
@@ -2,8 +2,10 @@ package com.api.base;
 
 import static io.restassured.RestAssured.*;
 
+import com.api.filters.LoggingFilter;
 import com.api.models.request.LoginRequest;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -18,6 +20,10 @@ public class BaseService {  //wrapper for rest assured
 	
 	private static final String BASE_URL = "http://64.227.160.186:8080";
 	private RequestSpecification requestSpecification;
+	
+	static {
+		RestAssured.filters(new LoggingFilter());
+	}
 	
 	public BaseService() {
 		requestSpecification = given().baseUri(BASE_URL);
--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,6 +1,7 @@
 package com.api.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.api.base.AuthService;
@@ -9,6 +10,7 @@ import com.api.models.response.LoginResponse;
 
 import io.restassured.response.Response;
 
+@Listeners(com.api.listeners.TestListener.class)
 public class LoginAPITest {
 	
 	@Test(description = "Verify if Login API is working..")
